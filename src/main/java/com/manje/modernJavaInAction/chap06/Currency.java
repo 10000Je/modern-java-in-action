38321eeeb01bac1be0d5750eee86befa497dd6db
10000Je <ssh030226@gmail.com>
2025-06-30 20:00:33 +0900

    ~7.1 병렬 스트림

---
+++ src/main/java/com/manje/modernJavaInAction/chap06/Currency.java
@@ -0,0 +1,5 @@
+package com.manje.modernJavaInAction.chap06;
+
+public enum Currency {
+    EUR, USD, JPY, GBP, CHF
+}
---
+++ src/main/java/com/manje/modernJavaInAction/chap07/Accumulator.java
@@ -0,0 +1,11 @@
+package com.manje.modernJavaInAction.chap07;
+
+public class Accumulator {
+
+    public long total = 0;
+
+    public void add(long value) {
+        total += value;
+    }
+
+}
